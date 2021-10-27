927c35527150dc31968f0ad87205a820da71bade
jinxin_fl <972784646@qq.com>
2021-10-27 10:56:44 +0800

    feat: 首页修改跳转按钮

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,20 @@
 import ProCard from '@ant-design/pro-card';
 import { Button } from 'antd';
 import React from 'react';
-import { findAllUser } from './services';
+import { useHistory } from 'react-router';
 
 const Home: React.FC = () => {
+  const history = useHistory();
   return (
     <>
       <ProCard>
         Home{' '}
         <Button
-          onClick={async () => {
-            await findAllUser();
+          onClick={() => {
+            history.push('/manager/menu');
           }}
         >
-          test
+          菜单管理
         </Button>
       </ProCard>
     </>
