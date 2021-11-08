db70de0cd4ed55f7acdd7a64abfb9852ae1f842e
jinxin_fl <972784646@qq.com>
2021-11-08 11:42:56 +0800

    feat: echart地图数据进行处理

--- src/pages/Home/mapOptions.ts
+++ src/pages/Home/mapOptions.ts
@@ -1,58 +1,45 @@
 import * as echarts from 'echarts';
 type EChartsOption = echarts.EChartsOption;
 
-const geoCoordMap = {
-  北京市: [116.405289, 39.904987],
-  天津市: [117.190186, 39.125595],
-  河北省: [114.502464, 38.045475],
-  山西省: [112.549248, 37.857014],
-  内蒙古自治区: [111.75199, 40.84149],
-  辽宁省: [123.429092, 41.796768],
-  吉林省: [125.324501, 43.886841],
-  黑龙江省: [126.642464, 45.756966],
-  上海市: [121.472641, 31.231707],
-  江苏省: [118.76741, 32.041546],
-  浙江省: [120.15358, 30.287458],
-  安徽省: [117.283043, 31.861191],
-  福建省: [119.306236, 26.075302],
-  江西省: [115.892151, 28.676493],
-  山东省: [117.000923, 36.675808],
-  河南省: [113.665413, 34.757977],
-  湖北省: [114.298569, 30.584354],
-  湖南省: [112.982277, 28.19409],
-  广东省: [113.28064, 23.125177],
-  广西壮族自治区: [108.320007, 22.82402],
-  海南省: [110.19989, 20.04422],
-  重庆市: [106.504959, 29.533155],
-  四川省: [104.065735, 30.659462],
-  贵州省: [106.713478, 26.578342],
-  云南省: [102.71225, 25.040609],
-  西藏自治区: [91.1145, 29.64415],
-  陕西省: [108.948021, 34.263161],
-  甘肃省: [103.83417, 36.06138],
-  青海省: [101.77782, 36.61729],
-  宁夏回族自治区: [106.23248, 38.48644],
-  新疆维吾尔自治区: [87.61688, 43.82663],
-  香港特别行政区: [114.16546, 22.27534],
-  澳门特别行政区: [113.54913, 22.19875],
-} as any;
-
 export const mapOptions = (
   data: { name: string; value: number }[],
 ): EChartsOption => {
-  const convertData = function (data: { name: string; value: number }[]) {
-    const res = [];
-    for (let i = 0; i < data?.length; i++) {
-      const geoCoord = geoCoordMap?.[data?.[i]?.name];
-      if (geoCoord) {
-        res.push({
-          name: data[i].name,
-          value: geoCoord.concat(data[i].value),
-        });
+  const getAddres = (addressName: string) => {
+    const tempArr = [];
+    for (let index = 0; index < ['省', '市', '区'].length; index++) {
+      const element = ['省', '市', '区'][index];
+      console.log(element, addressName.includes(element));
+
+      if (addressName.includes(element)) {
+        tempArr.push(element);
       }
     }
-    return res;
+    return tempArr;
   };
+
+  const getData = (res: { name: string; value: number }[]) => {
+    const newRes = res?.map((item) => {
+      const newName = item.name.split(/省|市|区/)[0] + getAddres(item.name)[0];
+      return { name: newName, value: item.value };
+    });
+
+    const obj: any = {};
+    const newarr: any[] = [];
+    newRes.map((item) => {
+      if (obj[item.name]) {
+        newarr.map((node) => {
+          if (item.name == node.name) {
+            node.value += item.value;
+          }
+        });
+      } else {
+        obj[item.name] = 1;
+        newarr.push(item);
+      }
+    });
+    return newarr;
+  };
+
   return {
     // 加载 amap 组件
     // amap: {
@@ -121,7 +108,7 @@ export const mapOptions = (
         zoom: 2,
         roam: true,
         map: 'china',
-        data: convertData(data),
+        data: getData(data),
         // symbolSize: function (val) {
         //   return val[2] < 5 ? val[2] * 3 : val[2];
         // },
