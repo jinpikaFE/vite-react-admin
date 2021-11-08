3748f1bb990b9875a3c3ead15284c67564cefc73
jinxin_fl <972784646@qq.com>
2021-11-08 15:32:05 +0800

    test: commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,23 @@
 module.exports = {
-    "env": {
-        "jest": true,
-        "browser": true,
-        "es2021": true,
-        "es7": true,
-        "node": true
+  env: {
+    jest: true,
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
 };
