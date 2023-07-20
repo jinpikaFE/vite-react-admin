e0e0c86acfbeae2111a797fa0454b3bd05ecbab0
jinxin <jinxin@hsmap.com>
2023-07-20 15:04:16 +0800

    feat: 性能指标枚举

--- src/apis/home/typing.d.ts
+++ src/apis/home/typing.d.ts
@@ -24,4 +24,21 @@ declare namespace Monitor {
     RECORDSCREEN = 'recordScreen',
     WHITESCREEN = 'whiteScreen'
   }
+
+  /** 性能指标名 */
+  enum PERFORMANCE_TYPE {
+    /** 上报长任务详情 */
+    LongTask = 'longTask',
+    /** 上报资源列表 */
+    ResourceList = 'resourceList',
+    /** 上报内存情况, safari、firefox不支持该属性 */
+    Memory = 'memory',
+    /** 首屏加载时间 */
+    FSP = 'FSP',
+    FCP = 'FCP',
+    LCP = 'LCP',
+    FID = 'FID',
+    CLS = 'CLS',
+    TTFB = 'TTFB'
+  }
 }
