be6a8a2b0fa31f41e628a80f9745dd619d83f2e3
jinxin <jinxin@hsmap.com>
2023-07-26 09:58:35 +0800

    feat: 首页监控页优化

--- src/apis/home/typing.d.ts
+++ src/apis/home/typing.d.ts
@@ -3,42 +3,4 @@ declare namespace Monitor {
     type: string
     apikey: string
   }
-
-  /**
-   * 事件类型
-   */
-  enum EVENTTYPES {
-    XHR = 'xhr',
-    FETCH = 'fetch',
-    CLICK = 'click',
-    HISTORY = 'history',
-    ERROR = 'error',
-    HASHCHANGE = 'hashchange',
-    UNHANDLEDREJECTION = 'unhandledrejection',
-    RESOURCE = 'resource',
-    DOM = 'dom',
-    VUE = 'vue',
-    REACT = 'react',
-    CUSTOM = 'custom',
-    PERFORMANCE = 'performance',
-    RECORDSCREEN = 'recordScreen',
-    WHITESCREEN = 'whiteScreen'
-  }
-
-  /** 性能指标名 */
-  enum PERFORMANCE_TYPE {
-    /** 上报长任务详情 */
-    LongTask = 'longTask',
-    /** 上报资源列表 */
-    ResourceList = 'resourceList',
-    /** 上报内存情况, safari、firefox不支持该属性 */
-    Memory = 'memory',
-    /** 首屏加载时间 */
-    FSP = 'FSP',
-    FCP = 'FCP',
-    LCP = 'LCP',
-    FID = 'FID',
-    CLS = 'CLS',
-    TTFB = 'TTFB'
-  }
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,79 +1,10 @@
-import { getMonitorList } from '@/apis/home'
-import ExcelTable from '@/components/exportExcel'
-import { getFirstDayOfMonth } from '@/utils/date'
-import { formatToDateTime } from '@/utils/dateUtil'
-import { ActionType } from '@ant-design/pro-components'
-import { useRef } from 'react'
+import PerformanceTable from './components/performanceTable'
 
 const Home: React.FC = () => {
-  const actionRef = useRef<ActionType>()
-
   return (
-    <ExcelTable
-      headerTitle="性能监控"
-      columns={[
-        {
-          title: '名称',
-          dataIndex: 'name',
-          hideInTable: true
-        },
-        {
-          title: '时间',
-          dataIndex: 'time',
-          hideInTable: true,
-          valueType: 'dateTimeRange',
-          initialValue: [getFirstDayOfMonth(new Date()), formatToDateTime(new Date())],
-          search: {
-            transform: val => ({
-              startTime: val?.[0],
-              endTime: val?.[1]
-            })
-          }
-        },
-        /** search */
-        {
-          title: '访问标识',
-          dataIndex: 'uuid',
-          hideInSearch: true,
-          ellipsis: true
-        },
-        {
-          title: '错误页面',
-          dataIndex: 'pageUrl',
-          hideInSearch: true,
-          ellipsis: true
-        },
-        {
-          title: '描述',
-          dataIndex: 'description',
-          hideInSearch: true
-        },
-        {
-          title: '时间',
-          dataIndex: '_time',
-          hideInSearch: true,
-          valueType: 'dateTime'
-        }
-      ]}
-      form={{
-        syncToUrl: false
-      }}
-      pagination={{
-        pageSize: 10
-      }}
-      rowKey="_time"
-      requestFn={async params => {
-        const data = await getMonitorList({
-          ...params,
-          type: 'performance',
-          apikey: 'vite-react-ts-admin'
-        })
-        return data
-      }}
-      actionRef={actionRef}
-      rowSelection={false}
-      toolBarRenderFn={() => []}
-    />
+    <>
+      <PerformanceTable />
+    </>
   )
 }
 
